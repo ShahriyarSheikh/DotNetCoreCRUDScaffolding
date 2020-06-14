72eab015c1300e40dc4c88a7d2e252ea274525b9
m.shahriyarsheikh <m.shahriyarsheikh@gmail.com>
2020-06-14 20:07:32 +0500

    first commit

---
+++ EmployeeData.Services/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeData.Services
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
